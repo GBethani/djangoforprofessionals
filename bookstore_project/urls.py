71ace9d87d992fef813b47f2b66b400e4e187c54
goel <jinkiller69@gmail.com>
2020-12-09 21:36:54 +0530

    Signup,Login,Logout,tests added

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+    # Django auth
+    path('accounts/',include('django.contrib.auth.urls')),
+    # local apps
+    path('accounts/',include('users.urls')),
     path('',include('pages.urls')),
 ]
