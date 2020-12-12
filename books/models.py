e847f15d6f86ed549acb84368332273fe02b902c
goel <jinkiller69@gmail.com>
2020-12-12 23:04:05 +0530

    Foreign keys,inlines,tests added

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 # Create your models here.
 class Book(models.Model):
@@ -13,3 +14,11 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book_detail',args=[str(self.pk)])
+
+class Review(models.Model):
+    book = models.ForeignKey(Book,on_delete=models.CASCADE,related_name='reviews')
+    review = models.TextField()
+    author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.review[:50]
