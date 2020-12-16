701e6299cfb2a89c22107892406b3faf88b91e00
goel <jinkiller69@gmail.com>
2020-12-16 12:21:47 +0530

    media files added

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -169,3 +169,5 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
